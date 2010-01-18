c9092476a7bdb29e8bcc4ec616dd42a9a2d18a9e
ThiefMaster <thiefmaster@gamesurge.net>
2010-01-18 23:04:24 +0800

    Add trust management (addtrust, deltrust, stats_trusted) Allow zero function arguments when calling on commandline

--- srvx.py
+++ srvx.py
@@ -361,6 +361,37 @@ class OpServ():
             return None
         return int(parts[4])
 
+    def addtrust(self, ip, count, duration, reason):
+
+        # Add a trusted host
+        response = self._command("addtrust %s %i %s %s" % (ip, int(count), duration, reason))
+        return response['data'][0][0:5] == 'Added'
+
+    def deltrust(self, ip):
+
+        # Remove a trusted host
+        response = self._command("deltrust %s" % ip)
+        return response['data'][0] == 'Removed trusted hosts from the trusted-hosts list.'
+
+    def stats_trusted(self, ip=None):
+
+        # Get a list of trusted hosts
+        trusts = []
+        response = self._command("stats trusted %s" % (ip or ""))
+
+        # 192.168.2.1 (limit 10; set 2 minutes and 41 seconds ago by cltx; expires 23 hours and 57 minutes: test bla)
+        for line in response['data'][1:]:
+            matches = re.match(r"^(\S+) \((limit (\d+)|no limit); set ([a-z0-9 ]+) ago by (\S+); expires ([^:]+): (.+)\)$", line)
+            trust = {'ip': matches.group(1),
+                     'limit': matches.group(2) != "no limit" and int(matches.group(3)) or 0,
+                     'set_time': matches.group(4),
+                     'setter': matches.group(5),
+                     'expires': matches.group(6),
+                     'reason': matches.group(7)}
+            trusts.append(trust);
+
+        return trusts;
+
 
 # Exceptions
 class AuthServAuthenticationFailure(Exception):
@@ -440,7 +471,9 @@ if __name__ == '__main__':
         logging.debug('Calling %s.%s' % (class_name, function_name))
 
         # Call the object function
-        if len(args) == 3:
+        if len(args) == 2:
+            print function()
+        elif len(args) == 3:
             print function(args[2])
         elif len(args) == 4:
             print function(args[2], args[3])
