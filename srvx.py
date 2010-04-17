5e7b2e38ffbed3748b2ae78f0b9778802eec1d10
MartinMartimeo <martin@martimeo.de>
2010-04-18 00:12:34 +0200

    Adding missing : and do not crae about case in oset password key

--- srvx.py
+++ srvx.py
@@ -368,11 +368,11 @@ class AuthServ():
     def oset(self, account, key=None, value=None):
 
         keys = ['color', 'email', 'info', 'language', 'privmsg', 'tablewith', 'width', 'maxlogins', 'password', 'flags', 'level', 'epithet']
-        if key.lower() not in keys
+        if key.lower() not in keys:
             return (False, '%s not in key list' % key)
 
         # Oset some things or grab them or grab them all
-        if key == 'password' and value:
+        if key.lower() == 'password' and value:
             response = self._command('oset *%s %s %s' % (account, key and key or "", value and value or ""), hide_arg=2)
         else:
             response = self._command('oset *%s %s %s' % (account, key and key or "", value and value or ""))
@@ -397,7 +397,7 @@ class AuthServ():
             return (False, response['data'][0])
 
         parts = response['data'][0].split(':')
-        if parts[0].lower() not in keys
+        if parts[0].lower() not in keys:
             return (False, response['data'][0])
 
         return (True, [parts[0],parts[1].strip()])
